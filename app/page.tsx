e4d14301d35d90f6e3475d78e548f0e48ea770a2
Ahsan Virk <ahsanlv@umich.edu>
2026-01-14 17:12:53 -0500

    Fix CSS import - move styles to globals.css per Next.js App Router requirements

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { useEffect } from 'react';
-import './styles.css';
 
 export default function HomePage() {
   useEffect(() => {
